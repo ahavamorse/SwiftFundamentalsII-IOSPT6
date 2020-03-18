8c02a5ea917b69c41e80c6f3617694a43256dc1c
HapiDani <hapidani@daniels-macbook-air.local>
2020-03-18 08:56:12 -0700

    Creates a class PizzaPlace that includes an array of pizzas

--- Contents.swift
+++ Contents.swift
@@ -29,5 +29,19 @@ struct Pizza {
 }
 
 let myPizza = Pizza(name: "49-er", cost: 12.99, category: .classic)
-
 print("I would like to order a \(myPizza.name)")
+
+class PizzaPlace {
+    var name: String
+    var address: String
+    var pizzas: [Pizza]
+    
+    // Create Initializer
+    init(name: String, address: String) {
+        self.name = name
+        self.address = address
+        self.pizzas = []
+    }
+}
+
+let johnnyPizzaPlace = PizzaPlace(name: "Johnny's Pizza", address: "123 Delicious Ave")
