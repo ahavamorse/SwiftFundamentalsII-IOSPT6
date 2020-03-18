e9cdb3007e151f3723b8cbb94d08cfa4c0b1924c
HapiDani <hapidani@daniels-macbook-air.local>
2020-03-18 08:49:57 -0700

    creates a struct for pizzas

--- Contents.swift
+++ Contents.swift
@@ -22,3 +22,12 @@ let myFavoritePizza: PizzaCategory = .classic
 
 print(myFavoritePizza)
 
+struct Pizza {
+    let name: String
+    let cost: Double
+    let category: PizzaCategory
+}
+
+let myPizza = Pizza(name: "49-er", cost: 12.99, category: .classic)
+
+print("I would like to order a \(myPizza.name)")
